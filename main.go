ce64d5a2ee974ec32b89db9be71fcd1a1400f9c1
aekkasit.k <aekkasit.kangyang14@gmail.com>
2023-12-22 13:26:57 +0700

    feat (✨): load config from .env

--- main.go
+++ main.go
@@ -3,19 +3,40 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	version string
+	build   string
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	port := getEnv("APP_PORT", "8080")
+
 	healthCheckHandler := func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Service is healthy!")
 	}
 
 	http.HandleFunc("/health", healthCheckHandler)
 
-	port := 8080
-	fmt.Printf("Application is running on :%d...\n", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	fmt.Printf("Application is running on port:%s Version:%s Build:%s\n", port, version, build)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		fmt.Printf("Error starting server: %s\n", err)
 	}
+}
+
+func getEnv(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
 }
